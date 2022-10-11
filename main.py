dde142812aa3e2f09d28f4983bb5f8f9519203e5
James Duarte <GarnetSunset@users.noreply.github.com>
2022-10-11 12:23:21 -0400

    Bring back the command line shit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from os import makedirs, listdir, remove, system
 from os.path import isfile, join, exists, splitext
 
+import ffmpeg
 from PyCriCodecs import USM
-from pyffmpeg import FFmpeg
 
 path = "R:/Downloads/PS5MovieUpgrade/moviesd"
 sofdec2enc = "R:/Documents/Programming/Tools/Sofdec2/sofdec2enc.exe"
@@ -18,14 +18,19 @@ for filename in listdir(path):
         usmObj.demux()
         usmObj.extract(extractPath)
         if not exists(f'{extractPath}/workspace/mov_conv_work/00000.avi'):
-            stream = FFmpeg(
-                f'{extractPath}/workspace/mov_conv_work/00000.ivf -qscale:v 0 {extractPath}/workspace/mov_conv_work/00000.avi')
+            stream = ffmpeg.input(f'{extractPath}/workspace/mov_conv_work/00000.ivf', **{"hwaccel": "nvdec"})
+            stream = ffmpeg.output(stream, f'{extractPath}/workspace/mov_conv_work/00000.avi', **{'qscale:v': 1})
+            ffmpeg.run(stream)
             if exists(f'{extractPath}/workspace/mov_conv_work/audio0.hca'):
-                stream = FFmpeg(
-                    f'-i {extractPath}/workspace/mov_conv_work/audio0.hca -filter_complex "channelmap=map=FL-FL|FR-FR|FC-BL|LFE-BR|BL-FC|BR-LFE:channel_layout=5.1" {extractPath}/workspace/mov_conv_work/audio0.wav')
+                system(
+                    f'ffmpeg -i {extractPath}/workspace/mov_conv_work/audio0.hca -filter_complex '
+                    f'"channelmap=map=FL-FL|FR-FR|FC-BL|LFE-BR|BL-FC|BR-LFE:channel_layout=5.1" '
+                    f'{extractPath}/workspace/mov_conv_work/audio0.wav')
             if exists(f'{extractPath}/workspace/mov_conv_work/audio1.hca'):
-                stream = FFmpeg(
-                    f'-i {extractPath}/workspace/mov_conv_work/audio1.hca -filter_complex "channelmap=map=FL-FL|FR-FR|FC-BL|LFE-BR|BL-FC|BR-LFE:channel_layout=5.1" {extractPath}/workspace/mov_conv_work/audio1.wav')
+                system(
+                    f'ffmpeg -i {extractPath}/workspace/mov_conv_work/audio1.hca -filter_complex '
+                    f'"channelmap=map=FL-FL|FR-FR|FC-BL|LFE-BR|BL-FC|BR-LFE:channel_layout=5.1" '
+                    f'{extractPath}/workspace/mov_conv_work/audio1.wav')
         if isfile(f'{extractPath}/workspace/mov_conv_work/00000.avi') and isfile(
                 f'{extractPath}/workspace/mov_conv_work/00000.ivf'):
             remove(f'{extractPath}/workspace/mov_conv_work/00000.ivf')
@@ -37,7 +42,7 @@ for filename in listdir(path):
                 remove(f'{extractPath}/workspace/mov_conv_work/audio1.hca')
             except:
                 pass
-        if isfile(f'{extractPath}/workspace/mov_conv_work/00000.avi'):
+        if isfile(f'{extractPath}/workspace/mov_conv_work/00000.avi') and not isfile(pathname):
             if isfile(f'{extractPath}/workspace/mov_conv_work/audio0.wav'):
                 createUsm = system(
                     f'{sofdec2enc} -br_range=0,60000000 -video00=\"{extractPath}/workspace/mov_conv_work/00000.avi\" -output=\"{pathname}\" -audio00=\"{extractPath}/workspace/mov_conv_work/audio0.wav\" -audio01=\"{extractPath}/workspace/mov_conv_work/audio1.wav\"')
